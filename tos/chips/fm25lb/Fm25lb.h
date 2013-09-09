d3968cea0c78e63db529ba15991a282dc27441c9
Brad Campbell <bradjc5@gmail.com>
2013-09-09 01:36:09 -0400

    addresses are 9 bits not 8

--- tos/chips/fm25lb/Fm25lb.h
+++ tos/chips/fm25lb/Fm25lb.h
@@ -5,9 +5,7 @@
 #ifndef __FM25LB_H__
 #define __FM25LB_H__
 
-//#include "Storage.h"
-
-typedef uint8_t fm25lb_addr_t;
-typedef uint8_t fm25lb_len_t;
+typedef uint16_t fm25lb_addr_t;
+typedef uint16_t fm25lb_len_t;
 
 #endif
