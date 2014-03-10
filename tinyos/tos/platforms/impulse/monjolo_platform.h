1f9f0209686aaaf0787b20818883e0f6c90bd47a
Brad Campbell <bradjc5@gmail.com>
2014-03-10 00:30:16 -0400

    conditionally set the monjolo version
    might also be set in the target file

--- tinyos/tos/platforms/impulse/monjolo_platform.h
+++ tinyos/tos/platforms/impulse/monjolo_platform.h
@@ -1,7 +1,9 @@
 #ifndef __MONJOLO_PLATFORM_H__
 #define __MONJOLO_PLATFORM_H__
 
+#ifndef MONJOLO_VERSION
 #define MONJOLO_VERSION 3
+#endif
 
 #endif
 
--- tinyos/tos/platforms/impulse2/monjolo_platform.h
+++ tinyos/tos/platforms/impulse2/monjolo_platform.h
@@ -2,7 +2,9 @@
 #define __MONJOLO_PLATFORM_H__
 
 // Identifier that describes what type of node this packet is from.
+#ifndef MONJOLO_VERSION
 #define MONJOLO_VERSION 3
+#endif
 
 #endif
 
